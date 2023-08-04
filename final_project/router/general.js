1d69d81dfd6ebb118c5311d11136bac9835621c0
Mario Masoud <mario.masoud@gmail.com>
2023-08-05 00:15:35 +0300

    completed with async-await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,47 +21,59 @@ public_users.post("/register", (req, res) => {
     return res.status(404).json({ message: "Unable to register user." });
 });
 
+//helper function to simulate asyn
+function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 // Get the book list available in the shop
-public_users.get("/", function (req, res) {
+public_users.get("/", async function (req, res) {
+    await delay(500);
     res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
+public_users.get("/isbn/:isbn", async function (req, res) {
     const isbn = req.params.isbn;
     const book = books[isbn];
 
     if (book) {
+        await delay(500);
         res.send(JSON.stringify(book, null, 4));
     } else {
+        await delay(1000);
         res.status(404).send({ message: "Book not found" });
     }
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
+public_users.get("/author/:author", async function (req, res) {
     const author = req.params.author;
     let filteredBooks = Object.values(books).filter(
         (book) => book.author === author
     );
 
     if (filteredBooks.length > 0) {
+        await delay(500);
         res.send(JSON.stringify(filteredBooks, null, 4));
     } else {
+        await delay(1000);
         res.status(404).send({ message: "Book not found" });
     }
 });
 
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
+public_users.get("/title/:title", async function (req, res) {
     const title = req.params.title;
     let filteredBooks = Object.values(books).filter(
         (book) => book.title === title
     );
 
     if (filteredBooks.length > 0) {
+        await delay(500);
         res.send(JSON.stringify(filteredBooks, null, 4));
     } else {
+        await delay(1000);
         res.status(404).send({ message: "Book not found" });
     }
 });
